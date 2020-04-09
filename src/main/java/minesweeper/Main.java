ae24654eb75ca35e319cc4b0f1865841a7d986f3
ljurak <lucas.jurak@gmail.com>
2020-04-09 15:13:35 +0200

    Refactor main class of the program

--- src/main/java/minesweeper/Main.java
+++ src/main/java/minesweeper/Main.java
@@ -1,34 +1,51 @@
 package minesweeper;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
 
+    private static final Pattern pattern = Pattern.compile("\\d+ \\d+ (mine|free)");
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("How many mines do you want on the field? ");
-        int mines = scanner.nextInt();
+        int mines = Integer.parseInt(scanner.nextLine());
         GameEngine game = new GameEngine(mines);
-        printBoard(game.getBoard());
+
+        printBoard(game.getUserBoard());
 
         boolean finished = false;
         while (!finished) {
-            int row;
-            int col;
-            boolean inputCorrect = true;
+            String input;
             do {
-                if (!inputCorrect) {
-                    System.out.println("There is a number here!");
-                }
+                System.out.print("Set/unset mines marks or claim a cell as free: ");
+                input = scanner.nextLine();
+            } while (!pattern.matcher(input).matches());
 
-                System.out.print("Set/delete mines marks (x and y coordinates): ");
-                col = scanner.nextInt();
-                row = scanner.nextInt();
-            } while (!(inputCorrect = game.markField(row - 1, col - 1)));
+            String[] inputParts = input.split(" ");
+            int col = Integer.parseInt(inputParts[0]);
+            int row = Integer.parseInt(inputParts[1]);
+            String mode = inputParts[2];
+
+            switch (mode) {
+                case "mine":
+                    game.markField(row - 1, col - 1);
+                    break;
+                case "free":
+                    game.revealField(row - 1, col - 1);
+                    break;
+                default:
+                    throw new IllegalStateException("Unsupported game mode");
+            }
 
             System.out.println();
-            printBoard(game.getBoard());
+            printBoard(game.getUserBoard());
+
+            if (game.isGameLost()) {
+                System.out.println("You stepped on a mine and failed!");
+                finished = true;
+            }
 
             if (game.checkVictory()) {
                 System.out.println("Congratulations! You found all mines!");
@@ -46,17 +63,21 @@ public class Main {
 
         System.out.print("--|");
         for (int i = 0; i < board.length; i++) {
-            System.out.printf("---");
+            System.out.print("---");
         }
         System.out.println("|");
 
         for (int i = 0; i < board.length; i++) {
             System.out.printf("%2d|", i + 1);
             for (int cell : board[i]) {
-                if (cell == -2) {
+                if (cell == -3) {
+                    System.out.print("  X");
+                } else if (cell == -2) {
                     System.out.print("  *");
-                } else if  (cell <= 0) {
+                } else if  (cell == -1) {
                     System.out.print("  .");
+                } else if (cell == 0) {
+                    System.out.print("  /");
                 } else {
                     System.out.printf("%3d", cell);
                 }
